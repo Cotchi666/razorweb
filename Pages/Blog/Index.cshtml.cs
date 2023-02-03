1d5bb49b013d741a4823523fe5cba2a13e7e1b3a
CotChi <chiennguyen.170921@gmail.com>
2023-02-03 22:03:09 +0700

    update paging

--- Pages/Blog/Index.cshtml.cs
+++ Pages/Blog/Index.cshtml.cs
@@ -19,13 +19,31 @@ namespace razorwebef.Pages.Blog
 
         public IList<Article> Article { get; set; } = default!;
 
-        [BindProperty(SupportsGet = true)]
+        public const int ITEMS_PER_PAGE = 1;
+
+        [BindProperty(SupportsGet = true, Name = "p")]
+        public int currentPage { get; set; }
+
+        public int countPages { get; set; }
+
         public string SearchString { get; set; }
         public async Task OnGetAsync()
         {
+            int totalArticle = await _context.Article.CountAsync();
+            countPages = (int)Math.Ceiling((double)totalArticle / ITEMS_PER_PAGE);
+
+            if (currentPage < 1)
+                currentPage = 1;
+            if (currentPage > countPages)
+                currentPage = countPages;
+
 
             // Truy vấn lấy các Article
-            var articles = from a in _context.Article orderby a.PublishDate descending select a;
+            var articles = (from a in _context.Article
+                            orderby a.PublishDate descending
+                            select a)
+                                .Skip((currentPage - 1) * ITEMS_PER_PAGE)
+                             .Take(ITEMS_PER_PAGE); ;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 Console.WriteLine(SearchString);
